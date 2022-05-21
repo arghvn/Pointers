5a889871ca65cde41c2ae41c27550208803970d4
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-21 15:47:59 +0430

    comments and output

--- pointers.go
+++ pointers.go
@@ -1,24 +1,56 @@
 package main
 
+//Go supports pointers, allowing you to pass references to values and records within your program.
+
 import "fmt"
 
+// how pointers work in contrast to values with 2 functions:
+//zeroval and zeroptr. zeroval has an int parameter, so arguments will
+// be passed to it by value. zeroval will get a copy of ival distinct from the one in the calling function
+
 func zeroval(ival int) {
-    ival = 0
+
+	ival = 0
+
 }
 
 func zeroptr(iptr *int) {
-    *iptr = 0
+
+	//zeroptr in contrast has an *int parameter, meaning that it takes an int pointer.
+	// The *iptr code in the function body then dereferences the pointer from its memory address
+	//to the current value at that address. Assigning a value to a dereferenced pointer changes the value at the referenced address.
+
+	*iptr = 0
+
 }
 
 func main() {
-    i := 1
-    fmt.Println("initial:", i)
 
-    zeroval(i)
-    fmt.Println("zeroval:", i)
+	i := 1
 
-    zeroptr(&i)
-    fmt.Println("zeroptr:", i)
+	fmt.Println("initial:", i)
 
-    fmt.Println("pointer:", &i)
+	zeroval(i)
+
+	fmt.Println("zeroval:", i)
+
+	zeroptr(&i)
+
+	//The &i syntax gives the memory address of i, i.e. a pointer to i.
+
+	//Pointers can be printed too
+
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &i)
+
 }
+
+//zeroval doesn’t change the i in main, but zeroptr does because
+// it has a reference to the memory address for that variable.
+
+//output :
+//initial: 1
+//zeroval: 1
+//zeroptr: 0
+//pointer: 0x42131100
